7528654cab413dcd7e3ecfbad1db870d6068a16a
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-05-18 21:34:59 +0700

    add asyn register and forgot-password

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -1,46 +1,69 @@
-import { createSlice } from "@reduxjs/toolkit"
-import { asyncLoginAction } from "../actions/auth"
+import { createSlice } from '@reduxjs/toolkit'
+import { asyncLoginAction, asyncRegisterAction, asyncForgotPasswordAction } from '../actions/auth'
 
 const initialState = {
-  token: "",
-  errorMessage: "",
-  warningMessage: "",
-  formError: [],
+    token: '',
+    succesMessage: '',
+    errorMessage: '',
+    warningMessage: '',
+    formError: [],
 }
 
 const authSilce = createSlice({
-  name: "auth",
-  initialState,
-  reducers: {
+    name: 'auth',
+    initialState,
+    reducers: {
     // login: (state, action) => {
     // 	state.token = action.payload
     // },
-    setErrorMessage: (state, action) => {
-      state.errorMessage = action.payload
+        setErrorMessage: (state, action) => {
+            state.errorMessage = action.payload
+        },
+        setWarningMessage: (state, action) => {
+            state.warningMessage = action.payload
+        },
+        clearMessage: (state) => {
+            state.errorMessage = ''
+            state.warningMessage = ''
+            state.succesMessage = ''
+        },
+        logout: () => {
+            return initialState
+        },
     },
-    setWarningMessage: (state, action) => {
-      state.warningMessage = action.payload
+    extraReducers: (builder) => {
+        builder.addCase(asyncLoginAction.rejected, (state, action) => {
+            if (typeof action.payload === 'string') {
+                state.errorMessage = action.payload
+            } else {
+                state.formError = action.payload
+            }
+        })
+        builder.addCase(asyncLoginAction.fulfilled, (state, action) => {
+            state.token = action.payload
+        })
+
+        builder.addCase(asyncRegisterAction.rejected, (state, action) => {
+            if (typeof action.payload === 'string') {
+                state.errorMessage = action.payload
+            } else {
+                state.formError = action.payload
+            }
+        })
+        builder.addCase(asyncRegisterAction.fulfilled, (state, action) => {
+            state.succesMessage = action.payload
+        })
+        builder.addCase(asyncForgotPasswordAction.rejected, (state, action) => {
+            if (typeof action.payload === 'string') {
+                state.errorMessage = action.payload
+            } else {
+                state.formError = action.payload
+            }
+        })
+        builder.addCase(asyncForgotPasswordAction.fulfilled, (state, action) => {
+            state.succesMessage = action.payload
+        })
     },
-    clearMessage: (state) => {
-      state.errorMessage = ""
-      state.warningMessage = ""
-    },
-    logout: () => {
-      return initialState
-    },
-  },
-  extraReducers: (builder) => {
-    builder.addCase(asyncLoginAction.rejected, (state, action) => {
-      if (typeof action.payload === "string") {
-        state.errorMessage = action.payload
-      } else {
-        state.formError = action.payload
-      }
-    })
-    builder.addCase(asyncLoginAction.fulfilled, (state, action) => {
-      state.token = action.payload
-    })
-  },
 })
 
 export const { logout, setErrorMessage, setWarningMessage, clearMessage } = authSilce.actions
