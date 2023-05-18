29ea02906c42346f8b07d4de57aea316905ac7f8
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-05-18 21:11:08 +0700

    add async register and forgot-password

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,24 +1,68 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import http from "../../helpers/http";
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import http from '../../helpers/http'
 
 export const asyncLoginAction = createAsyncThunk(
-  "auth/login",
-  async (payload, { rejectWithValue }) => {
-    try {
-      const body = new URLSearchParams(payload).toString();
-      const { data } = await http().post("/auth/login", body);
-      return data.results.token;
-    } catch (err) {
-      const results = err?.response?.data?.results;
-      const message = err?.response?.data?.message;
-      if (results) {
-        return rejectWithValue(results);
-      }
-      if (err.code === "ERR.NETWORK") {
-        return rejectWithValue("Error: Connection to backend failed");
-      }
-      // return rejectWithValue(message);
-      return rejectWithValue(message);
+    'auth/login',
+    async (payload, { rejectWithValue }) => {
+        try {
+            const body = new URLSearchParams(payload).toString()
+            const { data } = await http().post('/auth/login', body)
+            return data.results.token
+        } catch (err) {
+            const results = err?.response?.data?.results
+            const message = err?.response?.data?.message
+            if (results) {
+                return rejectWithValue(results)
+            }
+            if (err.code === 'ERR.NETWORK') {
+                return rejectWithValue('Error: Connection to backend failed')
+            }
+            // return rejectWithValue(message);
+            return rejectWithValue(message)
+        }
     }
-  }
-);
+)
+
+export const asyncRegisterAction = createAsyncThunk(
+    'auth/register',
+    async (payload, { rejectWithValue }) => {
+        try {
+            const body = new URLSearchParams(payload).toString()
+            const { data } = await http().post('/auth/register', body)
+            return data.message
+        } catch (err) {
+            const results = err?.response?.data?.results
+            const message = err?.response?.data?.message
+            if (results) {
+                return rejectWithValue(results)
+            }
+            if (err.code === 'ERR.NETWORK') {
+                return rejectWithValue('Error: Connection to backend failed')
+            }
+            // return rejectWithValue(message);
+            return rejectWithValue(message)
+        }
+    }
+)
+
+export const asyncForgotPasswordAction = createAsyncThunk(
+    'auth/forgotPassword',
+    async (payload, { rejectWithValue }) => {
+        try {
+            const body = new URLSearchParams(payload).toString()
+            const { data } = await http().post('/auth/forgotPassword', body)
+            return data.message
+        } catch (err) {
+            const results = err?.response?.data?.results
+            const message = err?.response?.data?.message
+            if (results) {
+                return rejectWithValue(results)
+            }
+            if (err.code === 'ERR.NETWORK') {
+                return rejectWithValue('Error: Connection to backend failed')
+            }
+            // return rejectWithValue(message);
+            return rejectWithValue(message)
+        }
+    }
+)
