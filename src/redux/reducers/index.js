da71fc513ded4bcbbe122b30b27e388ee84d5df0
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-05-19 19:06:36 +0700

    add index reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,15 +1,15 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import { combineReducers } from '@reduxjs/toolkit'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-import authReducer from "./auth";
+import authReducer from './auth'
 
 const authConfig = {
-  key: "auth",
-  storage,
-};
+    key: 'auth',
+    storage,
+}
 const reducer = combineReducers({
-  auth: persistReducer(authConfig, authReducer),
-});
+    auth: persistReducer(authConfig, authReducer),
+})
 
-export default reducer;
+export default reducer
