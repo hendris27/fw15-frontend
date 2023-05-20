91eff062447b219040bdd8b2bbd2b6f4f5312bba
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-05-20 23:53:34 +0700

    update formatter eslint

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,13 +1,13 @@
-import axios from "axios";
+import axios from 'axios'
 
 const http = (token) => {
-  const headers = {};
-  if (token) {
-    headers.Authorization = `Bearer ${token}`;
-  }
-  return axios.create({
-    headers,
-    baseURL: import.meta.env.VITE_BACKEND_URL,
-  });
-};
-export default http;
+    const headers = {}
+    if (token) {
+        headers.Authorization = `Bearer ${token}`
+    }
+    return axios.create({
+        headers,
+        baseURL: import.meta.env.VITE_BACKEND_URL,
+    })
+}
+export default http
