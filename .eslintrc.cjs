939911795c5f55d3c4c52a077e2a3f84d0e31499
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-07-18 17:19:23 +0700

    change setting linter format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,15 @@
 module.exports = {
-    env: { browser: true, es2020: true },
-    extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react/jsx-runtime",
-        "plugin:react-hooks/recommended",
-    ],
-    parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-    settings: { react: { version: "18.2" } },
-    plugins: ["react-refresh"],
-    rules: {
-        "react-refresh/only-export-components": "warn",
-        "indent": ["error", 4],
-        "linebreak-style": ["error", "unix"],
-        "quotes": ["error", "single"],
-        "semi": ["error", "never"],
-        "jsx-quotes": ["error", "prefer-single"]
-    },
-}
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+  },
+};
