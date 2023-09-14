e511026c152f85a0e69196e02b2c860a862bc494
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-09-14 22:31:50 +0700

    add componet img

---
+++ src/components/Image.jsx
@@ -0,0 +1,12 @@
+import propTypes from 'prop-types'
+
+const Image = ({ src, defaultImg, ...rest }) => {
+    return <img {...rest} src={src ? (src.startsWith('https') ? src : `${import.meta.env.VITE_BACKEND_URL}/uploads/${src}`) : defaultImg} alt='' />
+}
+
+Image.propTypes = {
+    src: propTypes.string,
+    defaultImg: propTypes.node,
+}
+
+export default Image
